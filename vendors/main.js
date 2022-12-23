faa7980387dc4060a0fb5ace6524f59867cebba9
akbarsharofiddinov <akbarsharofiddinov@gmail.com>
2022-12-23 08:50:57 -0800

    Appland Project has uploaded again and fix problems

---
+++ vendors/main.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".gallery-slider", {
+    slidesPerView: 7,
+    spaceBetween: 30,
+    centeredSlides: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".testimonials", {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
